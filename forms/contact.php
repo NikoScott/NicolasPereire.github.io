6a4cc8f38eb7224c4bf586bf64e79c944f6768b6
NikoScott <nico63@live.fr>
2023-03-20 23:13:18 +0100

    a

--- forms/contact.php
+++ forms/contact.php
@@ -1,20 +1,23 @@
 <?php
-if(isset($_POST['email'])) {
- 
-    // Récupération des données du formulaire
-    $name = $_POST['name'];
-    $email = $_POST['email'];
-    $message = $_POST['message'];
- 
-    // Construction du message à envoyer
-    $to = "nico.pereire@gmail.com";
-    $subject = "Nouveau message de $name";
-    $body = "Nom : $name\n\nE-mail : $email\n\nMessage : $message";
- 
-    // Envoi du message
-    mail($to, $subject, $body);
- 
-    // Message de confirmation
-    echo "Merci pour votre message ! Je vous réponds dès que possible.";
-}
+//   // Vérifier que le formulaire a été soumis
+//   if ($_SERVER["REQUEST_METHOD"] == "POST") {
+//     // Récupérer les données du formulaire
+//     $nom = $_POST["nom"];
+//     $email = $_POST["email"];
+//     $subject = $_POST["subject"];
+//     $message = $_POST["message"];
+
+//     // Envoyer un e-mail avec les données du formulaire
+//     $destinataire = "nico.pereire@gmail.com";
+//     $sujet = "Nouveau message de formulaire";
+//     $contenu = "Nom : " . $nom . "\n"
+//              . "E-mail : " . $email . "\n"
+//              . "Sujet : " . $subject
+//              . "Message : " . $message;
+//     mail($destinataire, $sujet, $contenu);
+
+//     // Rediriger l'utilisateur vers une page de confirmation
+//     header("");
+//     exit;
+//   }
 ?>
