c2c35ff261383d4b76c67b268efcd95295af1990
NikoScott <nico63@live.fr>
2023-04-21 10:55:03 +0200

    formspree

--- forms/contact.php
+++ forms/contact.php
@@ -1,27 +1,20 @@
 <?php
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-  // récupérer les champs du formulaire
-  $name = $_POST['name'];
-  $email = $_POST['email'];
-  $subject = $_POST['subject'];
-  $message = $_POST['message'];
+  // Vérifier que le formulaire a été soumis
+  if ($_POST) {
+    // Récupérer les données du formulaire
+    $nom = $_POST["name"];
+    $email = $_POST["email"];
+    $subject = $_POST["subject"];
+    $message = $_POST["message"];
 
-  // construction du message
-  $msg = "Nom : $name\n";
-  $msg .= "Email : $email\n";
-  $msg .= "Sujet : $subject\n";
-  $msg .= "Message : \n$message";
+    // Envoyer un e-mail avec les données du formulaire
+    $destinataire = "nico.pereire@gmail.com";
+    $sujet = "Nouveau message du portfolio";
+    $contenu = "Nom : " . $nom . "\n"
+             . "E-mail : " . $email . "\n"
+             . "Sujet : " . $subject
+             . "Message : " . $message;
+    mail($destinataire, $sujet, $contenu);
 
-  // destinataire de l'e-mail
-  $to = "nico.pereire@gmail.com";
-
-  // envoi de l'e-mail
-  if (mail($to, $subject, $msg)) {
-    $error = "";
-    echo "<script>alert('Votre message a été envoyé, merci !');</script>";
-  } else {
-    $error = "Une erreur s'est produite lors de l'envoi de votre message.";
-    echo "<script>alert('$error');</script>";
   }
-}
 ?>
